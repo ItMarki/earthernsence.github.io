4e05c45d27036488442590221fff99da5877ffc3
merp101 <34143165+merp101@users.noreply.github.com>
2018-01-15 20:59:43 -0500

    buying stuff

--- folder/game.js
+++ folder/game.js
@@ -3,6 +3,31 @@
 var player = {
   money: 10, //current errors
   mps: 0, //errors per second
-  //other stuff
+  firstCost: 10,
+  secondCost: 100,
+  thirdCost: 1000,
+  firstAmount: 0,
+  secondAmount: 0,
+  thirdAmount: 0,
+}
+const GEN_TIERS = ['first','second','third']; // can add more if more gens
 
+function buyGen(tier) {
+  var level = GEN_TIERS[tier];
+  if (player.money - player[level + "Cost"] >= 0) {
+    player[level + "Amount"] ++;
+    player.money -= player[level + "Cost"];
+  }
+}
+
+document.getElementById('gen1').onclick = function() {
+  buyGen(0);
+}
+
+document.getElementById('gen2').onclick = function() {
+  buyGen(1);
+}
+
+document.getElementById('gen3').onclick = function() {
+  buyGen(2);
 }

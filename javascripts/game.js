655a618a23cbc854944e819b795c1ca68ff45fbc
e666666 <a0939712328@gmail.com>
2018-06-30 14:00:47 +0800

    Fixed upg14 not working (how many bugs did i fixed?)

--- javascripts/game.js
+++ javascripts/game.js
@@ -491,7 +491,7 @@ function getMultTier(tier) {  let ret = new Decimal.pow(((player.downtimeChallen
   if (haveUpg(10)&&tier==8) ret = ret.mul(Math.pow(1.15,Math.sqrt(player.compAmount[7])))
   if (haveUpg(11)&&tier==9) ret = ret.mul(Math.pow(1.15,Math.sqrt(player.compAmount[8])))
   if (haveUpg(12)) ret = ret.mul(Math.pow(1.05,Math.sqrt(player.compAmount.reduce((a, b) => a + b, 0))))
-  if (haveUpg(13)&&tier<5) ret = ret.mul(10)
+  if (haveUpg(14)) ret = ret.mul(5)
   if (haveUpg(16)) ret = ret.mul(1000000)
   if (player.warningUpgrades.includes(1)) ret = ret.mul(getUpgradeMultiplier(1))
   if (player.warningUpgrades.includes(2)) ret = ret.mul(getUpgradeMultiplier(2))
